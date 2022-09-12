b5df7b4ab94610e4d0c7549d55e2607840ba7fd9
Ayoade Ibrahim <ayoadeibrahim84@gmail.com>
2022-09-12 13:38:10 -0700

    a program that prints all possible different combinations of two digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print 00 to 99 with no duplicate combo
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int firstDigit;
+	int lastDigit;
+	int secondDigit;
+
+	for (firstDigit = '0'; firstDigit <= '9'; firstDigit++;)
+	{
+		for (secondDigit = (firstDigit + 1); secondDigit <= '9'; secondDigit++;)
+		{
+			for (lastDigit = (secondDigit + 1); lastDigit <= '9'; lastDigit++;)
+			{
+				putchar(firstDigit);
+				putchar(secondDigit)
+				putchar(lastDigit);
+
+				if (firstDigit != '7' || secondDigit != '8' || lastDigit != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
